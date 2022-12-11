ecd350b522912c4420868f63162facffa5182793
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-12-11 17:30:40 +0600

    signup

--- my-app/pages/product.js
+++ my-app/pages/product.js
@@ -1,26 +1,9 @@
-import styles from "../styles/product.module.css";
-import Link from "next/link"
 
-import CustomDropdown from 'components/CustomDropdown/CustomDropdown.jsx';
-import Badge from 'components/Badge/Badge.js';
 
 function product ()  {
     return (
     <>
-    <div>
-      <CustomDropdown
-        buttonText="Regular"
-        dropdownList={[
-          "Action",
-          "Another action",
-          "Something else here",
-          {divider: true},
-          "Separated link",
-          {divider: true},
-          "One more separated link",
-        ]}
-      />
-    </div>
+    <h1>hello</h1>
 
     </>
     )
