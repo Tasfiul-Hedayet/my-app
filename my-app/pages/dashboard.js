99964f3479e12ec2c217e378a63ee16172804c17
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2023-01-08 02:56:05 +0600

    updated

--- my-app/pages/dashboard.js
+++ my-app/pages/dashboard.js
@@ -13,11 +13,9 @@ function dashboard () {
         <button>
         <Link href="/inventory">Settings</Link>
         </button>
-        <br/>
         <button>
         <Link href="/customer">Customer</Link>
         </button>
-        <br/>
         <button>
         <Link href="/payment">Payment</Link>
         </button>
@@ -25,13 +23,14 @@ function dashboard () {
         <button>
         <Link href="/purchase">Purchase</Link>
         </button>
-        <br/>
         <button>
         <Link href="/invoice">Invoice</Link>
         </button>
-        <br/>
         <button>
         <Link href="/product">Product</Link>
+        </button>
+        <button>
+        <Link href="/">Logout</Link>
         </button>
     </div>
     </>
